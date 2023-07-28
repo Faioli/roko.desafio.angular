c88df9d50cb69d49ed02a61b42acb7d58f759744
Henrique Faioli <Henrique.Faioli@UAPNET.COM>
2023-07-28 08:49:23 -0300

    Tratamento de erro

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -10,17 +10,19 @@ import { Cep } from './models/cep';
 export class AppComponent {
   cepInput: string = '';
   enderecos: Cep[] = [];
+  errorMessage: string = '';
 
   constructor(private cepService: CepService) {}
 
   buscarEnderecos() {
+    this.errorMessage = ''; // Limpa a mensagem de erro a cada nova busca
     const ceps = this.cepInput.split(';').map(cep => cep.trim());
     const observables = ceps.map(cep => this.cepService.getEndereco(cep));
     this.enderecos = [];
     observables.forEach(observable => {
       observable.subscribe(
         (cep: Cep) => this.enderecos.push(cep),
-        error => alert('Erro ao buscar endereço para o CEP: ' + error)
+        error => (this.errorMessage = 'Erro ao buscar endereço para o CEP, verifique os dados informados')
       );
     });
   }
